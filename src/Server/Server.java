10bc993c1572d69eaf73accbe341c8017bf28681
itzik <itzikd15@gmail.com>
2018-05-26 14:12:24 +0300

    Project 2 start

--- src/Server/Server.java
+++ src/Server/Server.java
@@ -1,8 +1,6 @@
 package Server;
 
-import Server.Strategies.IServerStrategy;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
+import Server.IServerStrategy;
 
 import java.io.IOException;
 import java.net.ServerSocket;
@@ -17,7 +15,6 @@ public class Server {
     private int listeningIntervalMS;
     private IServerStrategy serverStrategy;
     private volatile boolean stop;
-    private final Logger LOG = LogManager.getLogger(); //Log4j2
 
     public Server(int port, int listeningIntervalMS, IServerStrategy serverStrategy) {
         this.port = port;
@@ -29,31 +26,29 @@ public class Server {
         try {
             ServerSocket serverSocket = new ServerSocket(port);
             serverSocket.setSoTimeout(listeningIntervalMS);
-            LOG.info(String.format("Server started (port: %s, listening Interval: %s)",port,listeningIntervalMS));
             while (!stop) {
                 try {
                     Socket clientSocket = serverSocket.accept(); // blocking call
-                    LOG.info(String.format("Client excepted: %s",clientSocket.toString()));
                     try {
                         serverStrategy.applyStrategy(clientSocket.getInputStream(), clientSocket.getOutputStream());
                         clientSocket.getInputStream().close();
                         clientSocket.getOutputStream().close();
                         clientSocket.close();
                     } catch (IOException e) {
-                        LOG.error("IOException", e);
+                        System.out.println("IOException " + e);
                     }
                 } catch (SocketTimeoutException e) {
-                    LOG.debug("Socket Timeout - no Client requests!");
+                    System.out.println("Socket Timeout - no Client requests!");
                 }
             }
             serverSocket.close();
         } catch (IOException e) {
-            LOG.error("IOException", e);
+            System.out.println("IOException" + e);
         }
     }
 
     public void stop() {
-        LOG.info("Stopping server..");
+        System.out.println("Stopping server..");
         stop = true;
     }
 }
