17717db40f2e87332543cbf9fc1c9f278d3c7c0c
itzik <itzikd15@gmail.com>
2018-06-02 12:14:04 +0300

    Part B Half Done

--- src/Server/ServerStrategySolveSearchProblem.java
+++ src/Server/ServerStrategySolveSearchProblem.java
@@ -4,11 +4,15 @@ import algorithms.mazeGenerators.Maze;
 import algorithms.search.*;
 
 import java.io.*;
+import java.util.Properties;
 
 public class ServerStrategySolveSearchProblem implements IServerStrategy {
 
     @Override
     public void serverStrategy(InputStream inputStream, OutputStream outputStream) {
+        Properties prop = new Properties();
+        InputStream input = null;
+        File fileCheck = new File("config.properties");
         try {
             ObjectInputStream fromClient = new ObjectInputStream(inputStream);
             ObjectOutputStream toClient = new ObjectOutputStream(outputStream);
@@ -21,26 +25,35 @@ public class ServerStrategySolveSearchProblem implements IServerStrategy {
 //            System.out.println(System.getProperty("java.io.tmpdir"));
             String uniName = returnToClientMaze.toString(); //name of maze
 
-            File file = new File(tempDirectoryPath , uniName); //create file from maze name
+            File file = new File(tempDirectoryPath, uniName); //create file from maze name
             if (file.exists()) { //if it exist , solve gets the maze data
                 FileInputStream fIn = new FileInputStream(file);
                 ObjectInputStream FileToReturn = new ObjectInputStream(fIn);
                 solve = (Solution) FileToReturn.readObject();
                 FileToReturn.close();
-            }
-            else
-            {
+            } else {
+                String mazeTypeSerach = "BFS"; //default
+                if (fileCheck.length() != 0) { //if properties file empty, and hasnt been run yet
+                    input = new FileInputStream("config.properties");
+                    // load a properties file
+                    prop.load(input);
+                    mazeTypeSerach = prop.getProperty("mazeType"); //get number of cores from config file
+                }
                 //solve maze
                 SearchableMaze searchableMaze = new SearchableMaze(returnToClientMaze); // create a new searchable maze
+                if 
                 BreadthFirstSearch BFS = new BreadthFirstSearch();
-                solve = BFS.solve(searchableMaze);
+                DepthFirstSearch DFS = new DepthFirstSearch();
+                BestFirstSearch = new BestFirstSearch();
+                ASearchingAlgorithm Type = new;
+
+                solve = Type.solve(searchableMaze);
 
                 FileOutputStream fileOut = new FileOutputStream(file);
                 ObjectOutputStream objectReturn = new ObjectOutputStream(fileOut);
 
                 objectReturn.writeObject(solve);
                 objectReturn.flush();
-
 
 
             }
